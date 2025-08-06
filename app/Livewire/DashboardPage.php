abc7ff31b7562a1e6adb6e90dd09edff338af8c2
agung saputra <agung.dni19@gmail.com>
2025-08-06 22:22:19 +0700

    update dashboard

--- app/Livewire/DashboardPage.php
+++ app/Livewire/DashboardPage.php
@@ -6,6 +6,16 @@ use Livewire\Component;
 
 class DashboardPage extends Component
 {
+
+    public $count = [];
+
+    public function mount()
+    {
+        $this->count['matkul'] = \App\Models\MataKuliah::count();
+        $this->count['dosen'] = \App\Models\User::where('role', 'dosen')->count();
+        $this->count['peminatan'] = \App\Models\Peminatan::count();
+    }
+
     public function render()
     {
         return view('livewire.dashboard-page');
